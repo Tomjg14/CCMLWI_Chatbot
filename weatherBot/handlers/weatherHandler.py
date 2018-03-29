10948fa1aed001eef3eb9c47023857e961e68b68
MaxMoons <Max_Moons@Hotmail.com>
2018-03-29 13:16:41 +0200

    dictionary in weatherhandler geschoven

--- weatherBot/handlers/weatherHandler.py
+++ weatherBot/handlers/weatherHandler.py
@@ -1,9 +1,12 @@
 from pyowm import OWM
+import time
 
 class WeatherHandler:
 
     def __init__(self,OWMKEY):
         self.owm = OWM(OWMKEY)
+        self.weather_dict = {}   #value voor in dictionary
+        
 
     def getWeatherFromLoc(self,location):
         obs = self.owm.weather_at_coords(location["latitude"],location["longitude"])
@@ -23,4 +26,20 @@ class WeatherHandler:
         obs = self.owm.weather_at_id(city_id)
         return obs
 
-    
+    def get_specific_info(self,placename,infotype):
+        return self.weather_dict[placename][infotype]
+
+    def get_weather_forecast(self,placename):
+        return self.weather_dict[placename]
+
+    def set_weather_forecast(self,obs):
+        forecast = {}
+        w = obs.get_weather()
+        l = obs.get_location()
+        placename = str(l.getname())
+        forecast["wtime"] = self.localtime
+        forecast["status"] = str(w.get_detailed_status())
+        forecast["temperature"] = str(w.get_temperature('celsius').get('temp'))
+
+        self.weather_dict[placename] = forecast
+        
